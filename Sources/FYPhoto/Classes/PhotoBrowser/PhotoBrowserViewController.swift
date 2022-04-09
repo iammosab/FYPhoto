aaf488342ed66c924d23ce5077180ada040c254a
Moussaab Waziz <wazizmosab@gmail.com>
2022-04-09 15:40:28 +0000

    add function to PHAsset

--- Sources/FYPhoto/Classes/PhotoBrowser/PhotoBrowserViewController.swift
+++ Sources/FYPhoto/Classes/PhotoBrowser/PhotoBrowserViewController.swift
@@ -10,6 +10,32 @@ import Photos
 import MobileCoreServices
 import Foundation
 
+extension PHAsset {
+
+    func getURL(completionHandler : @escaping ((_ responseURL : URL?) -> Void)){
+        if self.mediaType == .image {
+            let options: PHContentEditingInputRequestOptions = PHContentEditingInputRequestOptions()
+            options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData) -> Bool in
+                return true
+            }
+            self.requestContentEditingInput(with: options, completionHandler: {(contentEditingInput: PHContentEditingInput?, info: [AnyHashable : Any]) -> Void in
+                completionHandler(contentEditingInput!.fullSizeImageURL as URL?)
+            })
+        } else if self.mediaType == .video {
+            let options: PHVideoRequestOptions = PHVideoRequestOptions()
+            options.version = .original
+            PHImageManager.default().requestAVAsset(forVideo: self, options: options, resultHandler: {(asset: AVAsset?, audioMix: AVAudioMix?, info: [AnyHashable : Any]?) -> Void in
+                if let urlAsset = asset as? AVURLAsset {
+                    let localVideoUrl: URL = urlAsset.url as URL
+                    completionHandler(localVideoUrl)
+                } else {
+                    completionHandler(nil)
+                }
+            })
+        }
+    }
+}
+
 /// use static method `create(photos:initialIndex:builder:)` to create PhotoBrowser instance
 public class PhotoBrowserViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     static let minimumLineSpacing: CGFloat = 20
